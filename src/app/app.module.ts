3e2d71a5c2a6ac914560943118a1b74bf6be6717
asjha2 <asjha2@cisco.com>
2021-06-05 15:19:03 +0530

    (refactor) : code refactor, separation of concern

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,17 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MaterialModule } from './material-module/material-module.module';
 import { ProductListComponent } from './product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    DialogContentExampleDialogComponent
+    DialogContentExampleDialogComponent,
+    NavbarComponent,
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { MaterialModule } from '../material-module/material-module.module'
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
